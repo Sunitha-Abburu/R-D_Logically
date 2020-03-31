0f1114d036cf19604eb407bf195e7f407057abeb
ma573 <62938360+ma573@users.noreply.github.com>
2020-03-31 17:47:52 +0530

    Update and rename CFRFWF.py to create_read_write_file.py

--- create_read_write_file.py
+++ create_read_write_file.py
@@ -4,22 +4,17 @@ input :set the path of text file
 output: Display the text
 '''
 
-
 def readfile(path):
     f = open(path, "r")
     print(f.read())
-
-
 '''
 process:create file text file 
 input :set path for creation
 output:Text file will be created
 '''
 
-
 def createfile(path):
     f = open(path, "w")
-
 
 '''
 process:exiting file add the data 
@@ -27,12 +22,10 @@ input :set data(text variable) to write and set path(path)
 Output: append data to text file
 '''
 
-
 def appenddatatofile(path, text):
     f = open(path, "a")
     f.write(text)
     f.close()
-
 
 '''
 process:CREATE file add the data 
@@ -40,12 +33,10 @@ input :set data(text variable) to write and set path(path)
 Output:File is created and data will write  
 '''
 
-
 def createfileandwrite(path, text):
     filehandle = open(path, 'w')
     filehandle.write(text)
     filehandle.close()
-
 
 path = 'C:/Users/user/Downloads/p1.txt'
 text = "TEXT TO WRITE OR APPEND"
